f0702b3b33e0cd4940981f3e8985bdf20c871331
Square <ansemlim79@gmail.com>
2023-10-03 18:30:06 +0800

    Tupple

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var person = {
     name: 'Todd',
-    age: 27
+    age: 27,
+    hobbies: ['Sports', 'Cooking'],
+    role: ['admin', 2] // add the role property here
 };
-console.log(person.name);
+console.log(person.role);
+var favoruteActivities;
+favoruteActivities = ['Sport,Cooking'];
+console.log(person.name + ' ' + person.age + ' ' + person.hobbies + favoruteActivities);
--- app.ts
+++ app.ts
@@ -1,9 +1,21 @@
 const person:{
     name: string,
     age: number,
-} = {
+    hobbies: string[]
+    role: [string, number]
+    }={
     name: 'Todd',
-    age: 27
+    age: 27,
+    hobbies: ['Sports','Cooking'],
+    role: ['number', 2]
 };	
+/* tupple role 
+    as we can see the role have been declare as string and number
+    so it will validate is the input of role is number and string.
+    note: it must be under same line.
+*/
 
-console.log(person.name);
+let favoruteActivities: string[];
+favoruteActivities = ['Sport,Cooking']
+
+console.log(person.name + ' ' + person.age + ' ' + person.hobbies + favoruteActivities);
