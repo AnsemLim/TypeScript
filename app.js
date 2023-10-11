7753163e4c8d5286455d3abe4166106fb5493c73
Square <ansemlim79@gmail.com>
2023-10-11 22:37:11 +0800

    Literal Type

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
+    /* Option 1
+    if (typeof input1 === 'number' && typeof input2 === 'number'){
         result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    if (resultConversion === 'as-number') {
+        return +result;
+    } else {
+        return result.toString();
+    }
+    */
+    /* Option 2 */
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2; // +input1 convert string to number
     }
     else {
-        result = input1.toString() + " " + input2.toString();
+        result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedName = combine('one', 'two');
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedName = combine('one', 'two', 'as-string');
 console.log(combinedName);
 /* In this case we see that need input number and string and both answer is correct.
     So to add another datatype use | 'union' 'or' */ 
--- app.ts
+++ app.ts
@@ -1,18 +1,37 @@
-function combine(input1: number | string,input2: number | string){ 
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text' // literal type
+    ){ 
     let result;
+    /* Option 1
     if (typeof input1 === 'number' && typeof input2 === 'number'){
         result = input1 + input2;
     } else {
-        result = input1.toString() + " " + input2.toString();
+        result = input1.toString() + input2.toString();
+    }
+    if (resultConversion === 'as-number') {
+        return +result;
+    } else {
+        return result.toString();
+    }
+    */
+   /* Option 2 */
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2; // +input1 convert string to number
+    } else {
+        result = input1.toString() + input2.toString();
     }
     return result;
-
 }
 
-const combinedAges = combine(30,26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedName = combine('one', 'two');
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedName = combine('one', 'two', 'as-text');
 console.log(combinedName);
 
 /* In this case we see that need input number and string and both answer is correct. 
