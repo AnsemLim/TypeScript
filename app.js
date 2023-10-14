4b890b169fbc3e039f1505d3c552afb27b97ec32
Square <ansemlim79@gmail.com>
2023-10-14 21:48:02 +0800

    Function Type Call Back Function

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    /* Option 1
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === 'as-number') {
-        return +result;
-    } else {
-        return result.toString();
-    }
-    */
-    /* Option 2 */
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2; // +input1 convert string to number
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedName = combine('one', 'two', 'as-text');
-console.log(combinedName);
-/* In this case we see that need input number and string and both answer is correct.
-    So to add another datatype use | 'union' 'or' */ 
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add; // store function in variable
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
